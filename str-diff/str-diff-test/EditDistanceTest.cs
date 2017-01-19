9f0b2f26161f1d263e7e68b2c734f8c717099726
Chris <code@cfoley.co.uk>
2017-01-19 21:33:11 +0000

    construct dynamic programming matrix

--- str-diff/str-diff-test/EditDistanceTest.cs
+++ str-diff/str-diff-test/EditDistanceTest.cs
@@ -10,12 +10,27 @@ namespace str_diff
     [TestClass]
     public class EditDistanceTest
     {
+        [TestMethod]
+        public void MatrixTest()
+        {
+            AssertMatrix("{0}", "", "");
+            AssertMatrix("{0,1|1,0}", "a", "a");
+            AssertMatrix("{0,1|1,1}", "a", "b");
+            AssertMatrix("{0,1,2|1,0,1|2,1,0}", "ab", "ab");
+            AssertMatrix("{0,1,2|1,0,1|2,1,1}", "ab", "ac");
+            AssertMatrix("{0,1,2|1,1,2|2,2,1}", "ab", "xb");
+            AssertMatrix("{0,1,2|1,1,2|2,2,2}", "ab", "xy");
+            AssertMatrix("{0,1|1,0|2,1}", "ab", "a");
+            AssertMatrix("{0,1,2|1,0,1}", "a", "ab");
+        }
+
         private void AssertMatrix(string expected, string a, string b)
         {
+            string message = string.Format("Diff {0} & {1}", a, b);
             EditDistance distance = new EditDistance(a, b);
             Diff diff = distance.Diff();
             string actual = DiffMatrixString(diff);
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, actual, message);
         }
 
         private string DiffMatrixString(Diff diff)
@@ -25,10 +40,5 @@ namespace str_diff
             return "{" + joined + "}";
         }
 
-        [TestMethod]
-        public void DiffTests()
-        {
-            AssertMatrix("{0}", "", "");
-        }
     }
 }
--- str-diff/str-diff/EditDistance.cs
+++ str-diff/str-diff/EditDistance.cs
@@ -19,8 +19,43 @@ namespace str_diff
 
         public Diff Diff()
         {
-            int[][] matrix = { new int[] { 0 } };
+            int[][] matrix = MakeMatrix();
             return new Diff(matrix);
+        }
+
+        private int[][] MakeMatrix()
+        {
+            int[][] result = InitialisMatrix();
+            PopulateMatrix(result);
+            return result;
+        }
+
+        private int[][] InitialisMatrix()
+        {
+            int[][] result = new int[a.Length + 1][];
+            for (int i = 0; i < result.Length; i++)
+                result[i] = new int[b.Length + 1];
+            for (int i = 0; i < result[0].Length; i++)
+                result[0][i] = i;
+            for (int i = 0; i < result.Length; i++)
+                result[i][0] = i;
+            return result;
+        }
+
+        private void PopulateMatrix(int[][] result)
+        {
+            for (int i = 0; i < a.Length; i++)
+                for (int j = 0; j < b.Length; j++)
+                {
+                    int best = Min(result[i][j], result[i + 1][j], result[i][j + 1]);
+                    int delta = a[i] == b[j] ? 0 : 1;
+                    result[i + 1][j + 1] = best + delta;
+                }
+        }
+
+        private int Min(int x, int y, int z)
+        {
+            return Math.Min(x, Math.Min(y, z));
         }
     }
 }
