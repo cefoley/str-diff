072e89e813c6b6972faa77232ad5687633082495
Chris <code@cfoley.co.uk>
2017-01-21 15:08:32 +0000

    Extracted EditStringBuilder from Diff

--- str-diff/str-diff/Diff.cs
+++ str-diff/str-diff/Diff.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 
 namespace str_diff
@@ -29,53 +28,7 @@ namespace str_diff
 
         public string EditStringA()
         {
-            StringBuilder result = new StringBuilder();
-            int i = matrix.Length - 1;
-            int j = matrix[0].Length - 1;
-            while (i > 0 && j > 0)
-            {
-                int nextMin = Math.Min(matrix[i - 1][j], matrix[i][j - 1]);
-                if (matrix[i - 1][j - 1] == matrix[i][j] && matrix[i][j] <= nextMin)
-                {
-                    result.Append("=");
-                    i--;
-                    j--;
-                } else if (matrix[i][j-1] == nextMin)
-                {
-                    result.Append("+");
-                    j--;
-                } else if (matrix[i-1][j] == nextMin)
-                {
-                    result.Append("-");
-                    i--;
-                }
-            }
-            while (i > 0)
-            {
-                result.Append("-");
-                i--;
-            }
-            while (j > 0)
-            {
-                result.Append("+");
-                j--;
-            }
-            Reverse(result);
-            return result.ToString();
-        }
-
-        private static void Reverse(StringBuilder result)
-        {
-            int i = 0;
-            int j = result.Length - 1;
-            while (i < j)
-            {
-                char temp = result[i];
-                result[i] = result[j];
-                result[j] = temp;
-                i++;
-                j--;
-            }
+            return new EditStringBuilder(matrix, a, b).EditStringA();
         }
 
         public string EditStringB()
---
+++ str-diff/str-diff/EditStringBuilder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace str_diff
+{
+    class EditStringBuilder
+    {
+        private int[][] matrix;
+        private string a;
+        private string b;
+
+        public EditStringBuilder(int[][] matrix, string a, string b)
+        {
+            this.matrix = matrix;
+            this.a = a;
+            this.b = b;
+        }
+
+        public string EditStringA()
+        {
+            StringBuilder result = new StringBuilder();
+            int i = matrix.Length - 1;
+            int j = matrix[0].Length - 1;
+            while (i > 0 && j > 0)
+            {
+                int nextMin = Math.Min(matrix[i - 1][j], matrix[i][j - 1]);
+                if (matrix[i - 1][j - 1] == matrix[i][j] && matrix[i][j] <= nextMin)
+                {
+                    result.Append("=");
+                    i--;
+                    j--;
+                }
+                else if (matrix[i][j - 1] == nextMin)
+                {
+                    result.Append("+");
+                    j--;
+                }
+                else if (matrix[i - 1][j] == nextMin)
+                {
+                    result.Append("-");
+                    i--;
+                }
+            }
+            while (i > 0)
+            {
+                result.Append("-");
+                i--;
+            }
+            while (j > 0)
+            {
+                result.Append("+");
+                j--;
+            }
+            Reverse(result);
+            return result.ToString();
+        }
+
+        private static void Reverse(StringBuilder result)
+        {
+            int i = 0;
+            int j = result.Length - 1;
+            while (i < j)
+            {
+                char temp = result[i];
+                result[i] = result[j];
+                result[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+    }
+}
